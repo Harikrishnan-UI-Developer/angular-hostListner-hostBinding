fd3df493650b3615e1e743837f8e47056b37e7f4
Harikrishnan-UI-Developer <harisoft89@gmail.com>
2020-07-27 00:04:54 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, VERSION } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable, interval } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -10,6 +12,14 @@ export class AppComponent  {
   value: string;
   users = [{name:"hari", gender:"m"}, {name:"krishnan", gender:"m"}, {name:"suganya", gender:"f"}, {name:"mikul", gender:"m"}, {name:"sai", gender:"m"}]
 
+// date$ - it means to show this 'date$' property is observable. not a mandatory one.
+date$ = interval(1000).pipe( map(x => new Date()),take(10));
+
+jsonData = this.http.get('https://jsonplaceholder.typicode.com/todos/1')
+
+constructor(public http: HttpClient){
+
+}
 addUser(_data){
   this.users = [...this.users,{name: _data, gender:"m"}]
   console.log("this.users", this.users)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { HelloComponent } from './hello.component';
 import { AppendPipe, FilterPipe } from '../custome.pipe';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, HttpClientModule ],
   declarations: [ AppComponent, HelloComponent, AppendPipe, FilterPipe ],
   bootstrap:    [ AppComponent ]
 })
